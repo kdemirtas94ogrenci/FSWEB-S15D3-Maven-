60f8916379e0bcf22cdf3b3e9b0470074b90ef8a
Kübra Demirtaş <241110004@stu.gedik.edu.tr>
2025-05-24 12:06:03 +0300

    d3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,69 @@
 package org.example;
 
 
+import org.example.entity.Employee;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        List<Employee> employees = new LinkedList<>();
+
+        employees.add(new Employee(1, "Dogancan", "Kinik"));
+        employees.add(new Employee(2, "Kübra", "Demirtaş"));
+        employees.add(new Employee(3, "Burak", "Civelek"));
+        employees.add(new Employee(4, "Burak", "Civelek"));
+        employees.add(new Employee(5, "Burak", "Civelek"));
+        employees.add(new Employee(7, "Deniz", "Acay"));
+
+    }
+    public static List<Employee> findDuplicates(List<Employee> employees) {
+        List<Employee> duplicates = new LinkedList<>();
+        List<Employee> uniques = new LinkedList<>();
+
+        for (Employee emp : employees) {
+            if (emp == null)
+                continue;
+
+            if (uniques.contains(emp)) {
+                duplicates.add(emp);
+            } else {
+                uniques.add(emp);
+            }
+        }
+
+
+        return duplicates;
+    }
+
+    public static Map<Integer, Employee> findUniques(List<Employee> employees) {
+        Map<Integer, Employee> uniques = new HashMap<>();
+
+        for (Employee emp : employees) {
+            if (emp != null && !uniques.containsKey(emp.getId())) {
+                uniques.put(emp.getId(), emp);
+            }
+        }
+
+        return uniques;
+    }
+
+    public static List<Employee> removeDuplicates(List<Employee> employees) {
+        Map<Employee, Integer> counts = new HashMap<>();
+
+        for (Employee emp : employees) {
+            if (emp == null) continue;
+            counts.put(emp, counts.getOrDefault(emp, 0) + 1);
+        }
+
+        List<Employee> uniques = new LinkedList<>();
+        for (Map.Entry<Employee, Integer> entry : counts.entrySet()) {
+            if (entry.getValue() == 1) {
+                uniques.add(entry.getKey());
+            }
+        }
+
+        return uniques;
     }
 }
---
+++ src/main/java/org/example/entity/Employee.java
@@ -0,0 +1,39 @@
+package org.example.entity;
+
+import java.util.Objects;
+
+public class Employee {
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public Employee(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && Objects.equals(firstname, employee.firstname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstname);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+}
